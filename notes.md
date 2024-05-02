9ecfe37ce6a3b7f4c462ff53af71f3a5c023f94e
Michael J Erwin <michaeljameserwin@gmail.com>
2024-05-01 21:21:27 -0400

    Linear and Binary search

---
+++ notes.md
@@ -0,0 +1,50 @@
+# Rust DSA (the Data structure & algorithm kind of dsa)
+
+## Motivation
+We already did this once.
+But the videos are long and have background music and it was awhile ago and I was a lot worse at Rust back then (according to my highly skewed view of myself).
+So what if we could do a data structure in 2-3 hours and actually finished it one stream/video.
+Yay, content
+Yay, no background music in VODs so maybe can do other things with them?
+Also, great chance for me to practice more Rust and DSA stuff on a personal level (because I have to start interviewing in a couple months)
+
+## Concerns
+- Chat distracts me (tbh everything distracts me)
+- I still have skill issues
+- So not sure if I can actually finish a data structure in a single stream
+- I also want to explain some of what the data structure is for those who don't know. Maybe I need a time limit on that at the beginning of vid/stream.
+
+## Stipulations/Rules (I don't like those)
+- explain the data structure in five minutes
+- It's cheating obviously for me to just look at my old implementations of a data structure
+- Not going to look up code examples of the dsa that I'm coding (duh)
+- But it's also unlikely that I can finish one quickly if I have to re-remember on stream exactly what the data structure does (e.g. adding/removing from a BST) so it seems that a little pre-stream prep is good.
+- A few notes, perhaps shared on screen about the data structure and basic behavior explained in non-code
+
+## Format
+- pre-stream (but still possibly live): write intro of blog post
+- 5m reading introduction of blog post
+- Implement the data structure in < 3 hrs (hopefully)
+
+## What are we going to write
+- linear search (slice)
+- binary search (slice)
+- linked list
+- doubly linked list
+- FiFo queue (backed by linked list)
+- rawvec (growable array)
+- array list/vec
+- sorting algos (bubble, quick)
+- ring buffer
+- binary search tree
+- priority queue/min heap
+- hashmap
+- harder stuff (prefix tree/trie, b tree, r/b trees, avl, matrix stuff)
+
+## Blog posts
+- write.as
+- maybe it makes sense to write the explanatory/introductory section of the blog before stream
+
+### random stuff
+- use of radix tree in router: https://docs.rs/matchit/latest/matchit/
+- algo viz: https://www.cs.usfca.edu/~galles/visualization/ComparisonSort.html
---
+++ src/search.rs
@@ -0,0 +1,118 @@
+#![allow(dead_code)]
+/**
+Linear search and binary search
+Because of using recursive operations on slices and lack of TCO in Rust,
+the functions operating on slices can stack overflow on large slices
+*/
+fn linear_search<T, U, P>(mut it: U, p: P) -> Option<T>
+where T: PartialEq,
+      U: Iterator<Item=T>,
+      P: Fn(&T) -> bool,
+{
+    match it.next() {
+        Some(a) if p(&a) => Some(a),
+        Some(_) => linear_search(it, p),
+        None => None
+    }
+}
+
+fn linear_search_slice<T: PartialEq>(s: &[T], q: &T) -> Option<usize> {
+    do_linear_search_slice(s, q, 0)
+}
+
+fn do_linear_search_slice<T: PartialEq>(s: &[T], q: &T, i: usize) -> Option<usize> {
+    if i < s.len() {
+        if &s[i] == q {
+            Some(i)
+        } else {
+            do_linear_search_slice(s, q, i + 1)
+        }
+    } else {
+        None
+    }
+}
+
+fn binary_search<T>(s: &[T], q: &T) -> Option<usize>
+where T: PartialOrd + PartialEq + std::fmt::Debug
+{
+    do_binary_search(s, q, 0, s.len())
+}
+
+fn do_binary_search<T>(s: &[T], q: &T, low: usize, high: usize) -> Option<usize>
+where T: PartialOrd + PartialEq
+{
+    if low >= high { return None; }
+
+    let mid = (high + low) / 2;
+
+    if &s[mid] == q {
+        Some(mid)
+    } else if q < &s[mid] {
+        do_binary_search(s, q, low, mid)
+    } else {
+        do_binary_search(s, q, mid + 1, high)
+    }
+}
+
+fn binary_search_rec<T>(s: &[T], q: &T) -> Option<usize>
+where T: PartialOrd + PartialEq
+{
+    let mid = s.len() / 2;
+
+    if &s[mid] == q {
+        Some(mid)
+    } else if q < &s[mid] && s.len() > 1 {
+        binary_search_rec(&s[..mid], q)
+    } else if q > &s[mid] && s.len() > 1 && (mid + 1) < s.len() {
+        binary_search_rec(&s[mid+1..], q)
+            .and_then(|i| Some(i + mid + 1))
+    } else {
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn linear_search_works() {
+        let it = vec![1, 2, 3].into_iter();
+        assert_eq!(Some(3), linear_search(it, |&n| n == 3));
+        let it = vec![1, 2, 3].into_iter();
+        assert_eq!(None, linear_search(it, |&n| n == 4));
+    }
+
+    #[test]
+    fn linear_search_slice_works() {
+        let s = [1, 2, 3].as_slice();
+        assert_eq!(Some(2), linear_search_slice(s, &3));
+        assert_eq!(None, linear_search_slice(s, &4));
+    }
+
+    #[test]
+    fn binary_search_works() {
+        let s = [1, 3, 5, 8, 13, 21].as_slice();
+        assert_eq!(Some(1), binary_search(s, &3));
+        assert_eq!(Some(4), binary_search(s, &13));
+        assert_eq!(Some(5), binary_search(s, &21));
+        assert_eq!(None, binary_search(s, &2));
+        assert_eq!(None, binary_search(s, &0));
+        assert_eq!(None, binary_search(s, &23));
+    }
+
+    #[test]
+    fn binary_search_rec_works() {
+        let s = [1, 3, 5, 8, 13, 21].as_slice();
+        assert_eq!(Some(0), binary_search_rec(s, &1));
+        assert_eq!(Some(1), binary_search_rec(s, &3));
+        assert_eq!(Some(2), binary_search_rec(s, &5));
+        assert_eq!(Some(3), binary_search_rec(s, &8));
+        assert_eq!(Some(4), binary_search_rec(s, &13));
+        assert_eq!(Some(5), binary_search_rec(s, &21));
+        assert_eq!(None, binary_search_rec(s, &0));
+        assert_eq!(None, binary_search_rec(s, &2));
+        assert_eq!(None, binary_search_rec(s, &15));
+        assert_eq!(None, binary_search_rec(s, &23));
+    }
+}
